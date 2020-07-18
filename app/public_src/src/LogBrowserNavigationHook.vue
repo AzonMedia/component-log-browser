5ba09f3a4a428d5940dcc65c41af14deb5ebdc63
Vesko Kenashkov <vesko@azonmedia.com>
2020-07-18 17:34:58 +0300

    - added vue templates

---
+++ app/public_src/src/LogBrowserNavigationHook.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Log browser navigation hook</div>
+</template>
+
+<script>
+    export default {
+        name: "LogBrowserNavigationHook"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/public_src/src/LogBrowseradmin.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Log Browser</div>
+</template>
+
+<script>
+    export default {
+        name: "LogBrowser"
+    }
+</script>
+
+<style scoped>
+
+</style>
